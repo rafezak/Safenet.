c20e8c14edb1202d53c34031fbb778cac4ef8525
Anruina <anruiamv@hotmail.com>
2024-04-19 07:45:23 +0200

    added tip model

--- Safenet 2.0/Models/Tip.cs
+++ Safenet 2.0/Models/Tip.cs
@@ -8,5 +8,9 @@ namespace Safenet_2._0.Models
 {
     public class Tip
     {
+        public string? Subject { get; set; }
+        public string? Question { get; set; }
+        public string? Description { get; set; }
+
     }
 }
