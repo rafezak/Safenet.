dbed86896bcb8618089c042fcea6573ac826bdf2
Anruina <anruiamv@hotmail.com>
2024-04-19 07:22:22 +0200

    created Port model + Added Log entry class

---
+++ Safenet 2.0/Models/LogEntry.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Safenet_2._0.Models
+{
+    public class LogEntry
+    {
+    }
+}
--- Safenet 2.0/Models/Port.cs
+++ Safenet 2.0/Models/Port.cs
@@ -3,10 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NetFwTypeLib;
 
 namespace Safenet_2._0.Models
 {
-    internal class Port
+    public class Port
     {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string LocalPorts { get; set; }
+        public int Protocol {  get; set; }
+
+        //to log history
+        public List<LogEntry> LogEntries { get; set; }
+
     }
 }
