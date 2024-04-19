bdb747b1f891027b79ffb8a662b151701d67f533
Anruina <anruiamv@hotmail.com>
2024-04-19 08:14:34 +0200

    added load rules and save rules and GetAllFirewallRules action

--- Safenet 2.0/Data/DataAccess.cs
+++ Safenet 2.0/Data/DataAccess.cs
@@ -4,44 +4,61 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Safenet_2._0.Models;
-using NetFwTypeLib;
+using NetFwTypeLib; // Windows Firewall
+using System.Text.Json; // JSON Sterialization
+using System.Collections.ObjectModel;
+using System.IO;
 
 namespace Safenet_2._0.Data
 {
     public class DataAccess
     {
+        private const string FilePathFW = "firewall_rules.json";
 
         #region Firewall-Ports
-        public void LoadPortsFromJson()
+
+        //Load the Firewall Port Rules
+        public ObservableCollection<Port> LoadRules()
         {
-
-
+            try
+            {
+                string json = File.ReadAllText(FilePathFW);
+                return JsonSerializer.Deserialize<ObservableCollection<Port>>(json);
+            }
+            catch (FileNotFoundException)
+            {
+                // If the file is not found, return an empty collection
+                return new ObservableCollection<Port>();
+            }
         }
 
-        public void SavePortRuleToJson() 
+        //Save current rules
+        public void SaveRules(ObservableCollection<Port> rules)
         {
-        
-
-
+            string json = JsonSerializer.Serialize(rules, new JsonSerializerOptions { WriteIndented = true });
+            File.WriteAllText(FilePathFW, json);
         }
 
-        public void RefreshPortList()
+        public ObservableCollection<Port> GetAllFireWallRules()
         {
+            ObservableCollection<Port> rules = new ObservableCollection<Port>();
 
+            // get existing firewall rules from Windows firewall
+            INetFwPolicy2 firewallPolicy = (INetFwPolicy2)Activator.CreateInstance(Type.GetTypeFromProgID("HNetCfg.FwPolicy2"));
 
+            foreach(INetFwRule rule in firewallPolicy.Rules)
+            {
+                Port newRule = new Port
+                {
+                    Name = rule.Name,
+                    Description = rule.Description,
+                    LocalPorts = rule.LocalPorts,
+                    Protocol = rule.Protocol,
+                };
+                rules.Add(newRule);
+            }
+            return rules;
         }
-
-        private void NewPortRule() 
-        {
-        
-        }
-
-        public void RemovePortRule()
-        {
-
-
-        }
-
 
 
         #endregion
--- Safenet 2.0/ViewModel/MainViewModel.cs
+++ Safenet 2.0/ViewModel/MainViewModel.cs
@@ -11,16 +11,29 @@ namespace Safenet_2._0.ViewModel
     public class MainViewModel
     {
         public ObservableCollection<Port> Rules { get; set; }
-    
-        public MainViewModel() 
+
+        public MainViewModel()
         {
             //LoadRules();
 
         }
+        public void AddRule(string name, string description, string localPorts, int protocol)
+        {
+            var newRule = new Port
+            {
 
-    
-    
+                Name = name,
+                Description = description,
+                LocalPorts = localPorts,
+                Protocol = protocol
+
+            };
+
+            Rules.Add(newRule);
+            SaveRules();
+
+        }
+
+
     }
-
-
 }
