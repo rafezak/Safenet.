6b5d92fe52c6e3ed8fc0fa9384dd2a842a7fbf4b
Anruina <anruiamv@hotmail.com>
2024-04-19 07:08:04 +0200

    Created Regions

---
+++ Safenet 2.0/Data/DataAccess.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Safenet_2._0.Models;
+
+namespace Safenet_2._0.Data
+{
+    public class DataAccess
+    {
+
+        #region Firewall-Ports
+
+
+        #endregion
+
+
+        #region Block App 
+        #endregion
+
+        #region Tips
+        #endregion
+
+
+        #region Ticket
+        #endregion
+    }
+}
